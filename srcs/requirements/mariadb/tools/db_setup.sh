9be12b8f408e2fb4324f3afa2e987cfd4747cfdf
pderksen98 <pieter.derksen@hotmail.com>
2023-09-08 16:22:53 +0200

    small adjustments

--- srcs/requirements/mariadb/tools/db_setup.sh
+++ srcs/requirements/mariadb/tools/db_setup.sh
@@ -7,9 +7,14 @@ then
 
 	# Create the run directory for mysqld
 	mkdir -p /run/mysqld
+	# chown command changes the ownership of files/directories
+	# it's setting both the owner and group of /run/mysqld and /var/lib/mysql to mysql 
+	 the -R option makes this change recursively for every file and directory inside the specified path
 	chown -R mysql:mysql /run/mysqld
 	chown -R mysql:mysql /var/lib/mysql
 
+	# this is a script that initializes the MariaDB/MySQL database
+	# the --basedir specifies the base directory, and the --datadir specifies the directory for the database files
 	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql #initializes database
 
 # Creates a temporary file to store the SQL commands to be executed, creates the database and the users
@@ -25,20 +30,31 @@ cat << EOF > init.sql
 
 	CREATE DATABASE IF NOT EXISTS $DB_NAME;
 
-	CREATE USER '$WP_BG_LOGIN'@'%';
-	SET PASSWORD FOR '$WP_BG_LOGIN'@'%' = PASSWORD('$WP_BG_PW');
-	GRANT ALL PRIVILEGES ON wordpress.* TO '$WP_BG_LOGIN'@'%';
-	GRANT ALL ON wordpress.* to '$WP_BG_LOGIN'@'%';
+	CREATE USER '$WP_A_LOGIN'@'%';
+	SET PASSWORD FOR '$WP_A_LOGIN'@'%' = PASSWORD('$WP_A_PW');
+	GRANT ALL PRIVILEGES ON wordpress.* TO '$WP_A_LOGIN'@'%';
+	GRANT ALL ON wordpress.* to '$WP_A_LOGIN'@'%';
 	FLUSH PRIVILEGES;
 
-	CREATE USER '$WP_SG_LOGIN'@'%';
-	SET PASSWORD FOR '$WP_SG_LOGIN'@'%' = PASSWORD('$WP_SG_PW');
+	CREATE USER '$WP_U_LOGIN'@'%';
+	SET PASSWORD FOR '$WP_U_LOGIN'@'%' = PASSWORD('$WP_U_PW');
 EOF
 
+# This starts the mysqld process (the MariaDB server) with the mysql user 
+# The --bootstrap option is used to start the server in an "initialize-only" mode, which runs the commands from init.sql.
 mysqld --user=mysql --bootstrap < init.sql
 
 fi
 
 echo "MariaDB started"
 
-exec mysqld --user=mysql --console #starts database server in foreground
+#starts database server in foreground (--console)
+# exec command replaces the current shell process with the specified command (in this case, mysqld)
+exec mysqld --user=mysql --console 
+
+# script essentially helps to:
+# - set up and start the MariaDB server,
+# - ensuring that necessary directories exist, 
+# - database is initialized, 
+# - proper users and permissions are created, 
+# - the server is started in the foreground
--- srcs/requirements/wordpress/tools/wp_setup.sh
+++ srcs/requirements/wordpress/tools/wp_setup.sh
@@ -8,12 +8,12 @@ if [ ! -f /var/www/wordpress/wp-config.php ]
 then
 	cd /var/www/wordpress #go into volume
     wp core download --allow-root
-    wp config create --allow-root --dbname=$DB_NAME --dbuser=$WP_BG_LOGIN --dbpass=$WP_BG_PW --dbhost=mariadb --config-file=/var/www/wordpress/wp-config.php --skip-packages --skip-plugins
+    wp config create --allow-root --dbname=$DB_NAME --dbuser=$WP_A_LOGIN --dbpass=$WP_A_PW --dbhost=mariadb --config-file=/var/www/wordpress/wp-config.php --skip-packages --skip-plugins
 
     echo Created config
 
-	wp core install --allow-root --url=$WP_URL --admin_user=$WP_BG_LOGIN --admin_password=$WP_BG_PW --title=mywordpresssite --admin_email=pleasedonotemailme@42.fr
-	wp user create $WP_SG_LOGIN pleasedonotemailme2@42.fr --allow-root --user_pass=$WP_SG_PW;
+	wp core install --allow-root --url=$WP_URL --admin_user=$WP_A_LOGIN --admin_password=$WP_A_PW --title=mywordpresssite --admin_email=pleasedonotemailme@42.fr
+	wp user create $WP_U_LOGIN pleasedonotemailme2@42.fr --allow-root --user_pass=$WP_U_PW;
 	chown -R www-data:www-data /var/www/wordpress
 fi
 echo "Wordpress started"
